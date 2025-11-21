e7bbc44aee95c2f56e727c2ef1106dda869b7b27
Jilmer P. <77193911+jilpadua@users.noreply.github.com>
2025-11-21 21:16:48 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.MessageContent]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
